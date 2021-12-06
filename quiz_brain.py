f1cf04dc2a25ce45599305c260d0b147981cbdcd
tiiedye <tiiedye@gmail.com>
2021-12-06 14:39:18 -0800

    starting code for quiz brain

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_input = input(f"Q.{self.question_number}: {current_question.text} (T/F)?: ").lower()
+        return user_input
