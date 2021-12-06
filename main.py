50607b2861e030a58100ff26a52231fe47064e83
tiiedye <tiiedye@gmail.com>
2021-12-06 14:21:05 -0800

    creating the question bank from data.py and the Question class

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Quiz Game
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    new_question = Question(question['text'], question['answer'])
+    question_bank.append(new_question)
